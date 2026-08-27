03c7a8ebd511e69d2ad5f68c12f1e24133f35b2d
ialmario <ialmario826@gmail.com>
2026-08-27 16:59:34 -0700

    Add Lab1 Hello program and VSCode configs
    Add Lab1/Hello.cpp (simple "Hello World" demo using cout and printf) and its compiled binary Lab1/Hello.exe. Add VS Code configuration files (.vscode/c_cpp_properties.json, .vscode/launch.json, .vscode/settings.json) to configure gcc/g++ and gdb for debugging and set C_Cpp_Runner options. These files provide a basic lab setup for the semester.

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/*
+* {lab setup for the rest of the sem}
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
